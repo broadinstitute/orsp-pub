0eb4d23eeb4778f32dd9c64886b36a0295aba23f
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2018-12-13 18:07:44 -0300

    wip rest controller project

--- grails-app/controllers/org/broadinstitute/orsp/ProjectController.groovy
+++ grails-app/controllers/org/broadinstitute/orsp/ProjectController.groovy
@@ -1,10 +1,34 @@
 package org.broadinstitute.orsp
 
+import grails.transaction.Transactional
+import com.google.gson.Gson
+
+@Transactional(readOnly = true)
 class ProjectController {
 
     def pages() {
       render(view: "/newProject/index")
     }
 
-   
+    def index() {
+
+    }
+
+    def show() {
+    }
+
+    def create() {
+    }
+
+    def edit() {
+    }
+
+    def save() {
+    }
+
+    def update() {
+    }
+
+    def delete() {
+    }
 }
