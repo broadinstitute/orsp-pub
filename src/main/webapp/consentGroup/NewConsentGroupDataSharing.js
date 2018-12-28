dd1973575fb90a27500b9838e2803b9576dcf199
Nadya Lopez <nlopez@nlopez1.local>
2018-12-28 15:19:13 -0300

    Text ellipsis on long file names

--- src/main/webapp/consentGroup/NewConsentGroupDataSharing.js
+++ src/main/webapp/consentGroup/NewConsentGroupDataSharing.js
@@ -61,45 +61,50 @@ export const NewConsentGroupDataSharing = hh(class NewConsentGroupDataSharing ex
     }
 
     return (
-      WizardStep({ title: this.props.title, step: 4, currentStep: this.props.currentStep,
-       error: this.props.errors.sharingPlan || this.props.generalError,
-       errorMessage: this.props.generalError && this.props.errors.sharingPlan ? 'Please complete all required fields' : 'Please check previous steps' }, [
-       InputFieldRadio({
-          id: "radioSharingPlan",
-          name: "sharingPlan",
-          label: "What is your Data Sharing plan?",
-          moreInfo: "",
-          optionValues: ["controlled", "open", "none", "undetermined"],
-          optionLabels: ["Controlled Access", "Open Access", "No Sharing", "Data Sharing plan not yet determined"],
-          value: this.state.formData.sharingPlan,
-          onChange: this.handleRadio2Change,
-          required: true,
-          error: this.props.errors.sharingPlan,
-          errorMessage: "Required field"
-        }),
-        InputFieldText({
-          isRendered: this.state.formData.sharingPlan === "controlled",
-          id: "inputDatabaseControlled",
-          name: "databaseControlled",
-          label: "Name of Database(s) ",
-          moreInfo: "(Data Use LetterNR/link, consent or waiver of consent)",
-          value: this.state.formData.databaseControlled,
-          disabled: false,
-          required: false,
-          onChange: this.handleInputChange
-        }),
-        InputFieldText({
-          isRendered: this.state.formData.sharingPlan === "open",
-          id: "inputDatabaseOpen",
-          name: "databaseOpen",
-          label: "Name of Database(s) ",
-          moreInfo: "(Data Use LetterNR/link, consent or waiver of consent, or documentation from source that consent is not available but samples were appropriately collected and publicly available)",
-          value: this.state.formData.databaseOpen,
-          disabled: false,
-          required: false,
-          onChange: this.handleInputChange
-        })
-      ])
+      WizardStep({
+        title: this.props.title, step: 4, currentStep: this.props.currentStep,
+        error: this.props.errors.sharingPlan || this.props.generalError,
+        errorMessage: this.props.generalError && this.props.errors.sharingPlan ? 'Please complete all required fields' : 'Please check previous steps'
+      }, [
+          div({ className: "questionnaireContainer" }, [
+
+            InputFieldRadio({
+              id: "radioSharingPlan",
+              name: "sharingPlan",
+              label: "What is your Data Sharing plan?",
+              moreInfo: "",
+              optionValues: ["controlled", "open", "none", "undetermined"],
+              optionLabels: ["Controlled Access", "Open Access", "No Sharing", "Data Sharing plan not yet determined"],
+              value: this.state.formData.sharingPlan,
+              onChange: this.handleRadio2Change,
+              required: true,
+              error: this.props.errors.sharingPlan,
+              errorMessage: "Required field"
+            }),
+            InputFieldText({
+              isRendered: this.state.formData.sharingPlan === "controlled",
+              id: "inputDatabaseControlled",
+              name: "databaseControlled",
+              label: "Name of Database(s) ",
+              moreInfo: "(Data Use LetterNR/link, consent or waiver of consent)",
+              value: this.state.formData.databaseControlled,
+              disabled: false,
+              required: false,
+              onChange: this.handleInputChange
+            }),
+            InputFieldText({
+              isRendered: this.state.formData.sharingPlan === "open",
+              id: "inputDatabaseOpen",
+              name: "databaseOpen",
+              label: "Name of Database(s) ",
+              moreInfo: "(Data Use LetterNR/link, consent or waiver of consent, or documentation from source that consent is not available but samples were appropriately collected and publicly available)",
+              value: this.state.formData.databaseOpen,
+              disabled: false,
+              required: false,
+              onChange: this.handleInputChange
+            })
+          ])
+        ])
     )
   }
 });
