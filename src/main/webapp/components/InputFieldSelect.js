e1e7aba2b17670814a5eaabdac9c75c94bd424b2
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2018-12-31 11:28:56 -0300

    changed tag to hh

--- src/main/webapp/components/InputFieldSelect.js
+++ src/main/webapp/components/InputFieldSelect.js
@@ -10,15 +10,16 @@ export const InputFieldSelect = hh(class InputFieldSelect extends Component {
   render() {
     return (
       InputField({ label: this.props.label, moreInfo: this.props.moreInfo, error: this.props.error, errorMessage: this.props.errorMessage }, [
-        <Select
-          id={this.props.id}
-          name={this.props.name}
-          value={this.props.value}
-          className="inputFieldSelect"
-          onChange={this.props.onChange(this.props.id)}
-          options={this.props.options}
-        />
-      ]
+        h(Select({
+            id: this.props.id,
+            name: this.props.name,
+            value: this.props.value,
+            className: "inputFieldSelect",
+            onChange: this.props.onChange(this.props.id),
+            options: this.props.options
+          })
+          )
+        ]
       ))
   }
 });
