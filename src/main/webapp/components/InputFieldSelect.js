454d81df60fd555569010b61ab3ec9fbec76acd0
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2018-12-31 12:54:03 -0300

    minor fix

--- src/main/webapp/components/InputFieldSelect.js
+++ src/main/webapp/components/InputFieldSelect.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import React from 'react';
 import Select from 'react-select';
-import { hh } from 'react-hyperscript-helpers';
+import { hh, h } from 'react-hyperscript-helpers';
 import { InputField } from './InputField';
 import './InputField.css';
 
@@ -10,14 +10,14 @@ export const InputFieldSelect = hh(class InputFieldSelect extends Component {
   render() {
     return (
       InputField({ label: this.props.label, moreInfo: this.props.moreInfo, error: this.props.error, errorMessage: this.props.errorMessage }, [
-        h(Select({
+        h(Select,{
             id: this.props.id,
             name: this.props.name,
             value: this.props.value,
             className: "inputFieldSelect",
             onChange: this.props.onChange(this.props.id),
             options: this.props.options
-          })
+          }
           )
         ]
       ))
