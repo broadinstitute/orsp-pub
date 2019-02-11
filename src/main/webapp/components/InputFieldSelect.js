fbeda365618cc68d8debf9eee56bde0f0c3cc86b
LeoForconesi <lforcone@broadinstitute.org>
2019-02-11 11:31:16 -0300

    BTRX - Project Review - Fundings (#59)
    * fix approve edits
    
    * fix multiselect
    
    * validation
    
    * review handle
    
    * fix typo
    
    * fix compare version
    
    * add new funding after deleted
    
    * compare
    
    * disable edit button
    
    * pr review

--- src/main/webapp/components/InputFieldSelect.js
+++ src/main/webapp/components/InputFieldSelect.js
@@ -16,71 +16,12 @@ export const InputFieldSelect = hh(class InputFieldSelect extends Component {
     console.log(error, info);
   }
 
-  sortByKey = (array, key) => {
-    return array.sort(function (a, b) {
-      var x = a[key]; var y = b[key];
-      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
-  };
-
   isEdited = (current, futureValue) => {
-    let edited = false;
-    let future = undefined;
-
-    if (futureValue[0] === '') {
-      future = futureValue;
-    } else {
-      future = futureValue[0];
-    }
-
-    if (this.props.edit || this.props.edit === undefined) {
-      if (current.length !== future.length) {
-        edited = true;
-      }
-
-      current.forEach((element, index) => {
-        if (future[index] !== undefined) {
-          if (element.key !== future[index].key) {
-            edited = true;
-          }
-        }
-      });
-    }
-    return edited;
+    return current !== futureValue || current === undefined
   };
 
   render() {
-
-    let currentValue  = [];
-    let value = [];
-
-    let currentValues = [];
-
-    if (this.props.currentValue === undefined) {
-      currentValue.push("");
-    } else if (this.props.currentValue.length === 0){
-      currentValue.push("");
-    } else {
-      currentValue = this.props.currentValue;
-    }
-
-    if (this.props.value.length === 0) {
-      value.push("");
-    } else {
-      value.push(this.props.value);
-    }
-
-    currentValue.forEach(item => {
-      currentValues.push(item.label);
-    });
-
-    let currentKeys = this.sortByKey(currentValue, 'key');
-    let keys = this.sortByKey(value, 'key');
-
-    let currentValueStr = currentValues.join(',');
-
-    // verified if edited ...
-    const edited = this.isEdited(currentKeys, keys);
+    const edited = this.props.edit ? this.isEdited(this.props.currentValue.value, this.props.value.value) : false;
 
     return (
       InputField({
@@ -91,7 +32,7 @@ export const InputFieldSelect = hh(class InputFieldSelect extends Component {
         readOnly: this.props.readOnly,
         value: this.props.value,
         currentValue: this.props.currentValue,
-        currentValueStr: currentValueStr,
+        currentValueStr: this.props.currentValue.label,
         edited : edited
       }, [
           div({ className: "inputFieldSelectWrapper" }, [
@@ -99,11 +40,11 @@ export const InputFieldSelect = hh(class InputFieldSelect extends Component {
               id: this.props.id,
               index: this.props.index,
               name: this.props.name,
-              value: this.props.readOnly && (this.props.value === undefined || this.props.value === '') ? '--' : this.props.value,
+              value: this.props.value,
               className: "inputFieldSelect",
               onChange: this.props.onChange(this.props.index),
               options: this.props.options,
-              placeholder: this.props.placeholder,
+              placeholder: edited ? '--' : this.props.placeholder,
               isMulti: this.props.isMulti
             })
           ])
