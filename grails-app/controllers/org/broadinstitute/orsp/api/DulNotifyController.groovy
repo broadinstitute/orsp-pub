8a511c3ef44771d042bcf3c327e5611431c2f783
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2019-02-19 13:42:48 -0300

    wip send mail

--- grails-app/controllers/org/broadinstitute/orsp/api/DulNotifyController.groovy
+++ grails-app/controllers/org/broadinstitute/orsp/api/DulNotifyController.groovy
@@ -4,23 +4,33 @@ import grails.converters.JSON
 import groovy.util.logging.Slf4j
 import org.broadinstitute.orsp.AuthenticatedController
 import org.broadinstitute.orsp.NotifyArguments
+import org.broadinstitute.orsp.User
 
 @Slf4j
 class DulNotifyController extends AuthenticatedController{
 
     def sendNotifications() {
+        User user = userService.findUser('triveros')
+        // TODO pedir los links de cada formulario antes de enviar el email
+
         println 'notify'
-        notifyService.s
         println request.JSON['projectKey']
-        notifyService.sendDulFormLinkNotification(
-                new NotifyArguments(
-                        toAddresses: ["triveros@broadinstitute.org"],
-                        subject: "New Data Use Letter form" + issue.projectKey,
-                        issue: issue,
-                        user: getUser(),
-                        comment: params.comment
-                )
-        )
+        def issue = queryService.findByKey(params.id)
+        def usersNotif = ['lforcone@broadinstitute','triveros@broadinstitute.org' ]
+        def dulLinksForm = method.getLinks()
+
+        usersNotif.each {
+
+            notifyService.sendDulFormLinkNotification(
+                    new NotifyArguments(
+                            toAddresses: [it.toString()],
+                            subject: "New Data Use Letter form" + request.JSON['projectKey'],
+                            issue: issue,
+                            user: user,
+                            details: 'link'
+                    )
+            )
+        }
         render (['ok': 'ok'] as JSON)
     }
 }
