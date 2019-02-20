b28c45d1f7111682ba741c350fd6c3ff3782ed5c
leoforconesi <lforcone@broadinstitute.org>
2019-02-20 11:06:24 -0300

    remove space

--- src/main/webapp/consentGroup/NewConsentGroup.js
+++ src/main/webapp/consentGroup/NewConsentGroup.js
@@ -140,7 +140,7 @@ class NewConsentGroup extends Component {
         this.changeSubmitState();
         console.error(error);
         this.toggleSubmitError();
-    });
+      });
   };
 
   toggleSubmitError = () => {
