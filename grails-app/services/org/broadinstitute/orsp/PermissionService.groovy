26fd00f8d9b11510e22f08bcdfbe751bcbcc5c42
Greg Rushton <rushtong@users.noreply.github.com>
2019-03-15 12:15:45 -0300

    Update grails-app/services/org/broadinstitute/orsp/PermissionService.groovy
    Co-Authored-By: dags <diego.a.gil@gmail.com>

--- grails-app/services/org/broadinstitute/orsp/PermissionService.groovy
+++ grails-app/services/org/broadinstitute/orsp/PermissionService.groovy
@@ -10,7 +10,7 @@ class PermissionService implements UserInfo {
     UserService userService
 
     // get issue's collaborators as a List<String>
-    def getIssueCollaborators(issue) {
+    List<String> getIssueCollaborators(Issue issue) {
         def collaborators = issue.extraProperties.findAll ({ it.name == 'collaborator' }).collect { property -> property.value }
         collaborators
     }
