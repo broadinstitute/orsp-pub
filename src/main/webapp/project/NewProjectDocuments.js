02d0e16d105bc5249dd90b0239c1b316086cf108
wloforteBroad <wloforte@broadinstitute.org>
2019-04-24 17:26:54 -0300

    UI change

--- src/main/webapp/project/NewProjectDocuments.js
+++ src/main/webapp/project/NewProjectDocuments.js
@@ -145,18 +145,20 @@ export const NewProjectDocuments = hh(class NewProjectDocuments extends Componen
               remove: this.removeFile,
               reviewFlow: false
             }),
-            Panel({ title: "Broad Responsible Party (or Designee) Attestation*" }, [
-              p({}, 'I confirm that the information provided above is accurate and complete. The Broad researcher associated with the project is aware of this application, and I have the authority to submit it on his/her behalf.'),
-              p({}, '[If obtaining coded specimens/data] I certify that no Broad staff or researchers working on this project will have access to information that would enable the identification of individuals from whom coded samples and/or data were derived. I also certify that Broad staff and researchers will make no attempt to ascertain information about these individuals.'),
-              InputFieldCheckbox({
-                id: "ckb_attestation",
-                name: "attestation",
-                onChange: this.handleAttestationCheck,
-                label: "I confirm",
-                checked: this.props.formData.attestation,
-                required: true
-              }),
-              small({ isRendered: this.props.errors.attestation, className: "errorMessage" }, 'Required Field')
+            div({ style: { 'marginTop': '25px' }}, [
+              Panel({ title: "Broad Responsible Party (or Designee) Attestation*" }, [
+                p({}, 'I confirm that the information provided above is accurate and complete. The Broad researcher associated with the project is aware of this application, and I have the authority to submit it on his/her behalf.'),
+                p({}, '[If obtaining coded specimens/data] I certify that no Broad staff or researchers working on this project will have access to information that would enable the identification of individuals from whom coded samples and/or data were derived. I also certify that Broad staff and researchers will make no attempt to ascertain information about these individuals.'),
+                InputFieldCheckbox({
+                  id: "ckb_attestation",
+                  name: "attestation",
+                  onChange: this.handleAttestationCheck,
+                  label: "I confirm",
+                  checked: this.props.formData.attestation,
+                  required: true
+                }),
+                small({ isRendered: this.props.errors.attestation, className: "errorMessage" }, 'Required Field')
+              ])
             ])
           ])
         ])
