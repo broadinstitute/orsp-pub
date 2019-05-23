e75c15ce0ea52c5d5e2d4eb3c8712462c8c23b74
vernicavicario <vernicavicario@yahoo.com.ar>
2019-05-23 15:18:56 -0300

    add styles

--- src/main/webapp/components/ErrorHandler.js
+++ src/main/webapp/components/ErrorHandler.js
@@ -1,5 +1,15 @@
 import { Component, React } from 'react';
-import { h1 } from 'react-hyperscript-helpers';
+import { h1, div, h3, a } from 'react-hyperscript-helpers';
+
+
+const styles = {
+  errorTitle: {
+    marginTop: '50px'
+  },
+  errorText: {
+    marginBottom: '200px'
+  }
+};
 
 export default class ErrorHandler extends Component {
   constructor(props) {
@@ -17,6 +27,13 @@ export default class ErrorHandler extends Component {
   }
 
   render() {
-    return this.state.hasError ?  h1({}, ['Something went wrong. Please try again.']) : this.props.children
+   return this.state.hasError ?  
+    div({},[
+      h1({ style: styles.errorTitle }, ['Something went wrong. Please try again.']),
+      h3({ style: styles.errorText }, [
+        'If problem persists, please contact support: ',
+        a({ href: "orsp@broadinstitute.org", className: "link" }, 'orsp@broadinstitute.org')
+      ])
+    ]) : this.props.children
   }
 }
