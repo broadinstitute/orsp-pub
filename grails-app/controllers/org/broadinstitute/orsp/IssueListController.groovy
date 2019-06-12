1eb9a105dcfc9619c058428fb67735895cc28820
vernicavicario <vernicavicario@yahoo.com.ar>
2019-06-12 15:05:02 -0300

    fix redirection

--- grails-app/controllers/org/broadinstitute/orsp/IssueListController.groovy
+++ grails-app/controllers/org/broadinstitute/orsp/IssueListController.groovy
@@ -53,7 +53,7 @@ class IssueListController extends AuthenticatedController {
             List<Issue> issues = projectsForUser((String) params.assignee, (String) params.max)
             def items = issues.collect {
                 Map<String, Object> arguments = IssueUtils.generateArgumentsForRedirect(it, it.projectKey, null)
-                createLink(controller: arguments.get("controller"), params: arguments.get("params"), action: arguments.get("action"))
+                def url = createLink(controller: arguments.get("controller"), params: arguments.get("params"), action: arguments.get("action"))
                 [
                         url: url,
                         key: it.projectKey,
