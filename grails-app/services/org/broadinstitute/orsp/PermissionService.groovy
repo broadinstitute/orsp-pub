cb4733ee963e028fd2764ef84a80e395abd0b9d8
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2019-06-17 17:03:05 -0300

    minor spacing

--- grails-app/services/org/broadinstitute/orsp/PermissionService.groovy
+++ grails-app/services/org/broadinstitute/orsp/PermissionService.groovy
@@ -11,17 +11,17 @@ class PermissionService implements UserInfo {
 
     // get issue's collaborators as a List<String>
     List<String> getIssueCollaborators(Issue issue) {
-        issue.extraProperties.findAll ({ it.name == IssueExtraProperty.COLLABORATOR }).collect { property -> property.value }
+      issue.extraProperties.findAll ({ it.name == IssueExtraProperty.COLLABORATOR }).collect { property -> property.value }
     }
 
     // get issue's pms as a List<String>
     List<String> getIssuePMs(Issue issue) {
-        issue.extraProperties.findAll ({ it.name == IssueExtraProperty.PM }).collect { property -> property.value }
+      issue.extraProperties.findAll ({ it.name == IssueExtraProperty.PM }).collect { property -> property.value }
     }
 
     // get issue's pis as a List<String>
     List<String> getIssuePIs(Issue issue) {
-        issue.extraProperties.findAll ({ it.name == IssueExtraProperty.PI }).collect { property -> property.value }
+      issue.extraProperties.findAll ({ it.name == IssueExtraProperty.PI }).collect { property -> property.value }
     }
 
     // verifies if logged user belongs to some user list ....
@@ -34,7 +34,7 @@ class PermissionService implements UserInfo {
                 || isAdmin
                 || isViewer)
 
-        !userHasAccess
+      !userHasAccess
     }
 }
 
