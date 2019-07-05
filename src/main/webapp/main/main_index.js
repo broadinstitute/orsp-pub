8578d2862a5daf74943600fe85fb2932a2a01804
Diego Gil <dgil@belatrixsf.com>
2019-07-05 18:04:22 -0300

    minor fix - basename

--- src/main/webapp/main/main_index.js
+++ src/main/webapp/main/main_index.js
@@ -7,13 +7,14 @@ import Main from "./Main";
 import RolesManagement from "../rolesManagement/RolesManagement";
 import { BrowserRouter } from 'react-router-dom';
 
+// TODO: basename must be configured from application.yml contextPath
 ReactDOM.render(
   <ErrorHandler>
-    <BrowserRouter>
+    <BrowserRouter basename="/dev">
       <Switch>
-        <Route exact path='/dev/user/rolesManagement' render = {(routeProps) =>  <RolesManagement {...routeProps}/> }/>
-        <Route exact path='/dev/project/main' render = {(routeProps) => <Main {...routeProps}/> }/>
-        <Route exact path='/dev/newConsentGroup/main' render = {(routeProps) => <Main {...routeProps}/> }/>
+        <Route exact path='/user/rolesManagement' render = {(routeProps) =>  <RolesManagement {...routeProps}/> }/>
+        <Route exact path='/project/main' render = {(routeProps) => <Main {...routeProps}/> }/>
+        <Route exact path='/newConsentGroup/main' render = {(routeProps) => <Main {...routeProps}/> }/>
       </Switch>
     </BrowserRouter>
   </ErrorHandler>,
