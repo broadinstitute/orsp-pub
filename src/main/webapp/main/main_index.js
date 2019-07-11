6e850101bd97c3c626aaaadd98cbd2622f089708
leoforconesi <lforcone@broadinstitute.org>
2019-07-11 12:50:54 -0300

    propagate extra props in routes

--- src/main/webapp/main/main_index.js
+++ src/main/webapp/main/main_index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Switch } from "react-router-dom";
 import '../index.css';
 import ErrorHandler from '../components/ErrorHandler';
-import Main from "./Main";
-import RolesManagement from "../rolesManagement/RolesManagement";
 import { BrowserRouter } from 'react-router-dom';
+import Routes from "./Routes";
 
 ReactDOM.render(
   <ErrorHandler>
     <BrowserRouter basename= {component.contextPath}>
-      <Switch>
-        <Route path= {"/user/rolesManagement"} render = {(routeProps) =>  <RolesManagement {...routeProps}/> }/>
-        <Route path= {"/project/main"} render = {(routeProps) => <Main {...routeProps}/> }/>
-        <Route path= {"/newConsentGroup/main"} render = {(routeProps) => <Main {...routeProps}/> }/>
-      </Switch>
+      <Routes props={} />
     </BrowserRouter>
   </ErrorHandler>,
 
