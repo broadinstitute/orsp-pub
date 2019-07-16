9e3c683529cce5ec80fb6a17d011967e8860ef48
vernicavicario <vernicavicario@yahoo.com.ar>
2019-07-16 14:00:34 -0300

    fix api interceptor

--- grails-app/controllers/org/broadinstitute/orsp/ApiInterceptor.groovy
+++ grails-app/controllers/org/broadinstitute/orsp/ApiInterceptor.groovy
@@ -10,7 +10,24 @@ import grails.converters.JSON
 class ApiInterceptor implements UserInfo  {
 
     ApiInterceptor() {
-        match controller: 'report', action: 'funding'
+        match controller: 'clarification'
+        match controller: 'dulNotify'
+        match controller: 'fileHelper'
+        match controller: 'issueReview'
+        match controller: 'project'
+        match controller: 'sampleConsentLink'
+        match controller: 'infoLink'
+        match controller: 'consentGroup'
+        match controller: 'dataUse'
+        match controller: 'issueList'
+        match controller: 'history'
+        match controller: 'irb'
+        match controller: 'ne'
+        match controller: 'nhsr'
+        match controller: 'search'
+        match controller: 'status'
+        match controller: 'submission'
+        match controller: 'user'
     }
 
     @Override
