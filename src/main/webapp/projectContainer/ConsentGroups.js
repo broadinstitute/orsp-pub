5197b88c6b05dd95d193753276e1e185e177318a
leoforconesi <lforcone@broadinstitute.org>
2019-07-18 11:33:52 -0300

    WIP

--- src/main/webapp/projectContainer/ConsentGroups.js
+++ src/main/webapp/projectContainer/ConsentGroups.js
@@ -1,10 +1,40 @@
-import { Component, Fragment } from 'react';
-import { div, hh, h } from 'react-hyperscript-helpers';
+import React, { Component, Fragment } from 'react';
+import { div, hh, h, h1 } from 'react-hyperscript-helpers';
 import { ProjectMigration } from '../util/ajax';
 import { ConsentCollectionLink } from '../util/ajax';
 import { ConfirmationDialog } from '../components/ConfirmationDialog';
 import { RequestClarificationDialog } from "../components/RequestClarificationDialog";
 import { Spinner } from "../components/Spinner";
+import Collapse, { Panel } from 'rc-collapse';
+import 'rc-collapse/assets/index.css';
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+const arrowPath = 'M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88' +
+  '.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.' +
+  '6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-0.7 5.' +
+  '2-2L869 536.2c14.7-12.8 14.7-35.6 0-48.4z';
+function expandIcon({ isActive }) {
+  return (
+    <i style={{ marginRight: '.5rem' }}>
+      <svg
+        viewBox="0 0 1024 1024"
+        width="1em"
+        height="1em"
+        fill="currentColor"
+        style={{
+          verticalAlign: '-.125em',
+          transition: 'transform .2s',
+          transform: `rotate(${isActive ? 90 : 0}deg)`,
+        }}
+      >
+        <path d={arrowPath} p-id="5827"></path>
+      </svg>
+    </i>
+  );
+}
 
 export const ConsentGroups = hh(class ConsentGroups extends Component {
 
@@ -16,7 +46,10 @@ export const ConsentGroups = hh(class ConsentGroups extends Component {
       action: '',
       issueKey: '',
       consentKey: '',
-      showRequestClarification: false
+      showRequestClarification: false,
+      activeKey: ['4'],
+      accordion: false
+
     };
     this.loadConsentGroups = this.loadConsentGroups.bind(this);
   }
@@ -128,10 +161,47 @@ export const ConsentGroups = hh(class ConsentGroups extends Component {
     }
   }
 
+  onChange = (activeKey) => {
+    this.setState({
+      activeKey,
+    });
+  }
+
+  toggle = () => {
+    this.setState({
+      accordion: !this.state.accordion,
+    });
+  }
+
   render() {
+    const accordion = this.state.accordion;
+    const btn = accordion ? 'Mode: accordion' : 'Mode: collapse';
+    const activeKey = this.state.activeKey;
     return (
      h(Fragment, {}, [       
-      div({ dangerouslySetInnerHTML: { __html: this.state.content } }, []),
+      // div({ dangerouslySetInnerHTML: { __html: this.state.content } }, []),
+       h(Collapse,{
+         accordion: true,
+         onChange:this.onChange,
+         activeKey:activeKey,
+         expandIcon:expandIcon
+       }, [
+
+
+         <Panel header={`This is panel header `} key={1}>
+          <p>{text.repeat(1)}</p>
+         </Panel>,
+           <Panel header="hello" headerClass="my-header-class">this is panel content</Panel>,
+           <Panel header="title2">this is panel content2 or other</Panel>
+       ]),
+       // <Collapse accordion={true}>
+       //   <Panel header="hello" headerClass="my-header-class">this is panel content</Panel>
+       //   <Panel header="title2">this is panel content2 or other</Panel>
+       // </Collapse>
+
+
+
+
       ConfirmationDialog({
         closeModal: this.closeConfirmationModal,
         show: this.state.showConfirmationModal,
