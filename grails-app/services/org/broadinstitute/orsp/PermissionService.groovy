7ebfeeccf5e0cb41dca468574e79d6d8770f1a89
Diego Gil <diegogil@broadinstitute.org>
2019-07-22 09:31:53 -0300

    fixing merge error

--- grails-app/services/org/broadinstitute/orsp/PermissionService.groovy
+++ grails-app/services/org/broadinstitute/orsp/PermissionService.groovy
@@ -46,32 +46,3 @@ class PermissionService implements UserInfo {
         !userHasAccess
     }
 }
-
-// get issue's pis as a List<String>
-List<String> getIssuePIs(Map<String, List<String>> extraProperties) {
-List<String> pis = extraProperties.findAll ({ it.key == IssueExtraProperty.PI })
-.values().flatten().collect({ it -> it.toString() })
-pis
-}
-
-// verifies if logged user belongs to some user list ....
-Boolean issueIsForbidden(Issue issue, String userName, boolean isAdmin, boolean isViewer) {
-Map<String, List<String>> extraProperties = issue.extraPropertiesMap
-userHasIssueAccess(issue.reporter, extraProperties, userName, isAdmin, isViewer)
-}
-
-Boolean issueIsForbidden(IssueSearchItemDTO issue, String userName, boolean isAdmin, boolean isViewer) {
-Map<String, List<String>> extraProperties = issue.extraProperties
-userHasIssueAccess(issue.reporter, extraProperties, userName, isAdmin, isViewer)
-}
-
-Boolean userHasIssueAccess(String reporter, Map<String, List<String>> extraProperties, String userName, boolean isAdmin, boolean isViewer) {
-boolean userHasAccess = (reporter == userName
-|| getIssueCollaborators(extraProperties)?.contains(userName)
-|| getIssuePMs(extraProperties).contains(userName)
-|| getIssuePIs(extraProperties).contains(userName)
-|| isAdmin
-|| isViewer)
-!userHasAccess
-}
-}
