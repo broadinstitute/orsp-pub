7493a91a36b24589a070bd2698298898d50a46cf
Jaime David Bernales Gusbeth <jbernales@jbernales.local>
2019-08-19 15:02:39 -0300

    Data Use Restiction Edit Page Mockup

--- src/main/webapp/dataUse/DataUseRestrictionIndex.js
+++ src/main/webapp/dataUse/DataUseRestrictionIndex.js
@@ -1,8 +1,7 @@
 import { Component } from 'react';
 import { div } from 'react-hyperscript-helpers';
 import SampleCollectionLinks from './SampleCollectionLinks';
-//import DataUseRestriction from './DataUseRestriction';
-import DataUseRestrictionEdit from './DataUseRestrictionEdit';
+import DataUseRestriction from './DataUseRestriction';
 
 class DataUseRestrictionIndex extends Component {
 
@@ -13,9 +12,8 @@ class DataUseRestrictionIndex extends Component {
   render() {
      return (
        div({}, [ 
-        DataUseRestrictionEdit()
-        //DataUseRestriction(),
-        //SampleCollectionLinks()
+        DataUseRestriction(),
+        SampleCollectionLinks()
        ])
      )
   }
