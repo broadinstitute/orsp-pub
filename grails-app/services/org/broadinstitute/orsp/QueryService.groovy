a31d751ee3f33395075eb9e3a9eb75f169000a5d
vernicavicario <vernicavicario@yahoo.com.ar>
2019-08-20 14:28:32 -0300

    info link, filter by deleted docuements

--- grails-app/services/org/broadinstitute/orsp/QueryService.groovy
+++ grails-app/services/org/broadinstitute/orsp/QueryService.groovy
@@ -388,7 +388,8 @@ class QueryService implements Status {
         final session = sessionFactory.currentSession
         final String query =
                 ' select * from storage_document ' +
-                ' where consent_collection_link_id = :consentCollectionIds'
+                ' where consent_collection_link_id = :consentCollectionIds' +
+                ' and deleted = 0'
         final SQLQuery sqlQuery = session.createSQLQuery(query)
         final results = sqlQuery.with {
             addEntity(StorageDocument)
