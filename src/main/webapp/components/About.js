de93cebfb6c6b5fc392fe5c88433635e5eb0e568
wloforteBroad <wloforte@broadinstitute.org>
2019-08-30 10:49:33 -0500

    Address PR comments

--- src/main/webapp/components/About.js
+++ src/main/webapp/components/About.js
@@ -24,7 +24,7 @@ export const About = hh(class About extends Component {
           prev.logged = resp.data.session;
           return prev;
         })
-      }).catch(error => console.log(error));
+      }).catch(error => this.setState(() => { throw error; }));
   };
 
   componentDidMount(){
@@ -42,14 +42,14 @@ export const About = hh(class About extends Component {
         p({ style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}, [
           a({
             isRendered: this.state.logged && accessDetails,
-            href:"https://iwww.broadinstitute.org/sponsored-research/research-subject-protection/office-research-subject-protection"}, [
+            href:"https://iwww.broadinstitute.org/sponsored-research/research-subject-protection/office-research-subject-protection", target: "_blank"}, [
               "ORSP on the Broad Intranet"
           ]),
         ]),
         p({ style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}, [
           "The Broad's Office of Research Subject Protection (ORSP) is committed to helping our community adhere to " +
-          "federal regulations and institutional policies governing the protection of human subjects who make our" +
-          "research possible. To fulfill that mission, the Broad must ensure appropriate regulatory oversight and" +
+          "federal regulations and institutional policies governing the protection of human subjects who make our " +
+          "research possible. To fulfill that mission, the Broad must ensure appropriate regulatory oversight and " +
           "reliable documentation storage.  The ORSP Portal is an online platform where Broad staff members can upload " +
           "documents for ORSP review (either as stand-alone submissions, or in preparation for an IRB protocol applications), " +
           "store consent forms and regulatory approvals, and search for data use restrictions. Please contact ",
@@ -60,10 +60,10 @@ export const About = hh(class About extends Component {
         div({ isRendered: this.state.logged && accessDetails }, [
             h3({ style: { fontSize: styles.titleSize }}, ["User Guide"]),
             p({ style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }},[
-              "To access detailed instructions about how to use the ORSP portal, please visit:",
+              "To access detailed instructions about how to use the ORSP portal, please visit: ",
               br({}),
               a({
-                href:"https://intranet.broadinstitute.org/research-subject-protection/orsp-online-portal-submission-system"},
+                href:"https://intranet.broadinstitute.org/research-subject-protection/orsp-online-portal-submission-system", target: "_blank"},
                 ["https://intranet.broadinstitute.org/research-subject-protection/orsp-online-portal-submission-system"]
               )
             ])
