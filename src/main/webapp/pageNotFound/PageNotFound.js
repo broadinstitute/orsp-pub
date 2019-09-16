9da96bb48f467b4d5d5d1945955a20e58d8cb4ce
Tadeo Riveros Koloszwa <tadeoriverosk@gmail.com>
2019-09-16 16:33:37 -0300

    fixed react functional component

--- src/main/webapp/pageNotFound/PageNotFound.js
+++ src/main/webapp/pageNotFound/PageNotFound.js
@@ -1,13 +1,13 @@
-import { Component } from 'react';
+import React from 'react';
 
-export const NotFound = () => {
+export const PageNotFound = () => {
   return (
-    <div className={"row"} style= {"margin:60px 0"}>
-      <div className={"col-xs-12"}>
-        <h1>"Sorry, this page could not be found."</h1>
+      <div className={"row"} style={{margin: "60px 0"}}>
+        <div className={"col-xs-12"}>
+          <h1>Sorry, this page could not be found.</h1>
+        </div>
       </div>
-    </div>
   )
 };
 
-export default NotFound;
+export default PageNotFound;
