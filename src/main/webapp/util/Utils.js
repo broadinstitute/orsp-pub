c0600a31fe941c41903a8774a5940a77ff086c72
leoforconesi <lforcone@broadinstitute.org>
2019-09-18 14:58:49 -0300

    quick status fix

--- src/main/webapp/util/Utils.js
+++ src/main/webapp/util/Utils.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import moment from 'moment';
+import get from 'lodash/get';
+
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-import moment from "moment";
 
 export const validateEmail = (email) => {
   let valid = false;
@@ -11,6 +13,12 @@ export const validateEmail = (email) => {
     valid = true;
   }
   return valid;
+};
+
+export const projectStatus = (project) => {
+  const LEGACY = 'Legacy';
+  const issueStatus = get(project, 'approvalStatus', '');
+  return issueStatus !== LEGACY ? issueStatus : get(project, 'status', '');
 };
 
 export const areAllTheseThingsTruthy = (values) => {
