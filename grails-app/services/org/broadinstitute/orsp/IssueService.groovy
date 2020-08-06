fc893b9682aa371ac9c2a39776dffc45ebdc8ea1
Juan Orbegozo <you@example.com>
2020-08-06 15:55:13 -0300

    bugfix FK constraint

--- grails-app/services/org/broadinstitute/orsp/IssueService.groovy
+++ grails-app/services/org/broadinstitute/orsp/IssueService.groovy
@@ -585,7 +585,7 @@ class IssueService implements UserInfo {
             }
 
             List<ConsentCollectionLink> links = queryService.findAllConsentCollectionLink(projectKey)
-            links?.each { it.delete(hard: true) }
+            links?.each { it.delete(hard: true, flush: true) }
 
             Collection<IssueExtraProperty> issueExtraProperties = issue.getExtraProperties()
             issueExtraProperties?.each { it.delete(hard: true) }
