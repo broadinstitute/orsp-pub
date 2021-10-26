e0bfb1f600ef2b8ad68572d6691ab55c0eaaf12a
Nirmal <77339291+njohnybroad@users.noreply.github.com>
2021-10-26 19:24:05 +0530

    Update NewConsentGroupController.groovy (#712)
    adding additional info in Sample/data cohorts

--- grails-app/controllers/org/broadinstitute/orsp/api/NewConsentGroupController.groovy
+++ grails-app/controllers/org/broadinstitute/orsp/api/NewConsentGroupController.groovy
@@ -182,6 +182,41 @@ class NewConsentGroupController extends AuthenticatedController {
             UtilityClass.registerIssueMarshaller()
             try {
                 LinkedHashMap consentGroups = consentService.findProjectConsentGroups(params.projectKey)
+                
+                     for (Map.Entry<String, Object> entry : consentGroups.entrySet()) {
+                        String key = entry.getKey();
+                        Object value = entry.getValue();
+                        if(key == "consentGroups"){
+                            for (Object key1 : value) {
+//                                System.out.println(key1.getAt("summary")+","+key1.getAt("projectKey"));
+                                LinkedHashMap consentGroup = queryService.getConsentGroupByKey(key1.getAt("projectKey"))
+                                for (Map.Entry<String, Object> entry1 : consentGroup.entrySet()) {
+                                    String key2 = entry1.getKey();
+                                    if(key2 == "extraProperties"){
+                                        for (Object key3 : entry1.getValue()) {
+                                            String institute = key3.getAt("institutionalSources");
+                                            if(institute != null && institute != "[]" ){
+                                                int intIndex = institute.indexOf("country")
+
+                                                String sub = institute.substring(intIndex,institute.length()-3).substring(9).substring(1)
+                                                //    System.out.println(">>"+sub);
+
+                                                key1.putAt("summary",key1.getAt("summary")+' / '+key3.getAt("collInst")+' / '+sub)
+                                            }else{
+                                                key1.putAt("summary",key1.getAt("summary")+' / '+key3.getAt("collInst"))
+                                            }
+
+
+                                        }
+                                    }
+                                }
+
+                            }
+
+                        }
+
+                    }
+                
                 response.status = 200
                 JSON.use(UtilityClass.ISSUE_RENDERER_CONFIG) {
                     render( consentGroups as JSON)
