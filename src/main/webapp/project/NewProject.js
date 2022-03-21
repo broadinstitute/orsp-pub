c30f78b522e52694595a6fcc764685c15ed92e79
shajiparameswaran <45231997+shajiparameswaran@users.noreply.github.com>
2022-03-21 20:07:06 +0530

    Cric 1261 new (#750)
    * Update to funding.js
    
    * Update Fundings.js
    
    * Updating Sponsor field
    
    Sponsor field required for new projects
    
    * Update Fundings.js
    
    Made sponsor field required for new projects
    
    * Update Fundings.js
    
    Made sponsor field required for new peojects
    
    * Update Fundings.js
    
    Made sponsor field required
    
    * Update NewProject.js
    
    Sponsor and Identifier warning
    
    * Update NewProject.js
    
    Sponsor field warning
    
    * Update NewProject.js
    
    Sponsor field update
    
    * Update NewProject.js
    
    * Sponsor field update

--- src/main/webapp/project/NewProject.js
+++ src/main/webapp/project/NewProject.js
@@ -289,7 +289,11 @@ const NewProject = hh(class NewProject extends Component {
           fundings = true;
           isValid = false;
         }
-        if (!fundings && (funding.source.value === 'federal_prime' || funding.source.value === 'federal_sub-award') && isEmpty(funding.identifier) && isEmpty(funding.sponsor)) {
+        if (!fundings && (funding.source.value === 'federal_prime' || funding.source.value === 'federal_sub-award') && isEmpty(funding.identifier)) {
+          fundingAwardNumber = true;
+          isValid = false;
+        }
+        if (!funding.sponsor) {
           fundingAwardNumber = true;
           isValid = false;
         }
