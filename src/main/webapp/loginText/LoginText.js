e14d9e4c9efcb63296428f331ca9639008d6f0f8
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-08-03 18:19:23 +0530

    CRIC-1340 Login page text
    - Added new login text component
    - Added api's for getting and updating login page text

---
+++ src/main/webapp/loginText/LoginText.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import './Wizard.css'
+import './index.css'
+import { hh, h1, div } from 'react-hyperscript-helpers';
+
+import { InputFieldText } from '../components/InputFieldText'; 
+import { InputFieldTextArea } from '../components/InputFieldTextArea';
+import { LoginText } from '../util/ajax';
+import { AlertMessage } from '../components/AlertMessage';
+
+export const LogintText = hh(class LogintText extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            heading: '',
+            body: '',
+            currentValue: {},
+            alert: ''
+        };
+        this.init();
+    }
+
+    init() {
+        let current = {};
+        LoginText.getLoginText().then(loginText => {
+            console.log("Login Text: "+loginText);
+            current.heading = loginText.heading;
+            current.body = loginText.body;
+        });
+        this.setState(prev => {
+            prev.currentValue = current;
+            return prev;
+        })
+    }
+
+    handleHeadingChange(e) {
+        let value = e.target.value;
+        this.setState(prev => {
+            prev.heading = value;
+            return prev;
+        })
+    }
+
+    handleBodyChange(e) {
+        let value = e.target.value;
+        this.setState(prev => {
+            prev.body = value;
+            return prev;
+        })
+    }
+
+    submitEditResponses() {
+        let heading = this.state.heading;
+        let body = this.state.body;
+        console.log("heading: "+heading, "body: "+body);
+        LoginText.updateLoginText(heading, body).then(() => {
+            this.init();
+            this.setState(prev => {
+                prev.heading = '';
+                prev.body = '';
+                return prev;
+            })
+        }).catch(error => {
+            this.setState(prev => {
+                prev.alert = "We had an unexpected error "+error;
+                return prev;
+            })
+        })
+    }
+
+    render() {
+        return (
+            div({}, [
+                h1({ className: "wizardTitle" }, ["Login Text"]),
+                div({}, [
+                    InputFieldText({
+                        id: "LoginTextHeading",
+                        name: "heading",
+                        label: "Heading for login page text",
+                        value: this.state.heading,
+                        currentValue: this.state.currentValue.heading,
+                        required: "true",
+                        error: this.state.heading == '' ? true : false,
+                        errorMessage: "Heading cannot be empty",
+                        onChange: this.handleHeadingChange
+                    }),
+                    InputFieldTextArea({
+                        id: "loginTextBody",
+                        name: "body",
+                        label: "Body for login page text",
+                        value: this.state.body,
+                        currentValue: this.state.currentValue.body,
+                        required: "true",
+                        error: this.state.body == '' ? true : false,
+                        errorMessage: "Body cannot be empty",
+                        onChange: this.handleBodyChange
+                    }),
+                    div({ isRendered: this.state.readOnly === false, className: "buttonContainer", style: { 'margin': '0 0 0 0' } }, [
+                        button({
+                          className: "btn buttonPrimary floatRight",
+                          onClick: this.submitEditResponses(),
+                        }, ["Submit"])
+                    ]),
+                    AlertMessage({
+                        msg: this.state.alert,
+                        show: this.state.alert !== '' ? true : false,
+                        type: 'danger'
+                    })
+                ])
+            ])
+        );
+    }
+})
