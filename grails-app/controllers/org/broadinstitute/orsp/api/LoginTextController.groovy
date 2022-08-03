6da827f0f591aa770448688dd9d6cd3081431c10
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-08-03 18:14:13 +0530

    - Added login text controller, service and domain - Added get and update for login text

---
+++ grails-app/controllers/org/broadinstitute/orsp/api/LoginTextController.groovy
@@ -0,0 +1,35 @@
+package org.broadinstitute.orsp.api
+
+import grails.converters.JSON
+import org.broadinstitute.orsp.AuthenticatedController
+import org.broadinstitute.orsp.LoginText
+import org.broadinstitute.orsp.LoginTextService
+import org.broadinstitute.orsp.utils.IssueUtils
+
+import java.sql.SQLException
+
+class LoginTextController extends AuthenticatedController {
+
+    LoginTextService loginTextService
+
+    def getLoginText() {
+        try {
+            List<LoginText> loginText = loginTextService.getLoginText()
+            render loginText as JSON
+        } catch (Exception e) {
+            handleException(e)
+        }
+    }
+
+    def updateLoginText() {
+        Map<String, Object> loginText = IssueUtils.getJson(Map.class, request.JSON)
+        try {
+            LoginTextService.UpdateLoginText((String)loginText.get("heading"), (String)loginText.get("body"))
+            response.status = 200
+            render([message: 'Login text was updated'] as JSON)
+        } catch(Exception e) {
+            handleException(e)
+        }
+    }
+
+}
