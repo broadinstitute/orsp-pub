8dd52dc75ea0f8914c4c9f6b02c3c9d3f9757143
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-08-05 13:53:07 +0530

    Cric 1340 login text (#854)
    - Added login text controller, service and domain
    - Added get and update for login text
    - New login text edit component

--- grails-app/services/org/broadinstitute/orsp/LoginTextService.groovy
+++ grails-app/services/org/broadinstitute/orsp/LoginTextService.groovy
@@ -24,14 +24,9 @@ class LoginTextService {
     void UpdateLoginText(String heading, String body) throws SQLException {
         LoginText loginText = LoginText.findById(1)
         if (loginText != null) {
-            if(loginText.heading != null) {
-                loginText.setHeading(heading)
-                persistenceService.saveLoginText(loginText)
-            }
-            if (loginText.body != null) {
-                loginText.setBody(body)
-                persistenceService.saveLoginText(loginText)
-            }
+            loginText.setHeading(heading)
+//            loginText.setBody(body)
+            persistenceService.saveLoginText(loginText)
         } else {
             log.error("Error while trying to update login text")
         }
