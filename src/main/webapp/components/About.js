48d22719b28640b956625c7527cf65124d21d084
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-08-18 14:13:53 +0530

    Adding text editor

--- src/main/webapp/components/About.js
+++ src/main/webapp/components/About.js
@@ -36,7 +36,10 @@ export const About = hh(class About extends Component {
         let data = loginText.data[0];
         this.setState(prev => {
           prev.heading = data[1];
-          prev.body = data[2];
+          let bodyData = data[2];
+            bodyData = bodyData.replaceAll("&lt;", "<");
+            bodyData = bodyData.replaceAll("&gt;", ">");
+          prev.body = bodyData;
           return prev;
         })
       })
@@ -46,54 +49,93 @@ export const About = hh(class About extends Component {
 
     const showAccessDetails = !isEmpty(this.props.showAccessDetails) ? true : false;
     return (
-      div({className: "row"}, [
-        AlertMessage({
-          msg: 'You must be a Broad Institute User for further access. Please sign out and log in with a "broadinstitute.org" email account.',
-          show: Storage.userIsLogged() && !Storage.getCurrentUser().isBroad,
-          type: 'danger'
-        }),
+      <div className='row'>
+        <AlertMessage
+          msg='You must be a Broad Institute User for further access. Please sign out and log in with a "broadinstitute.org" email account.'
+          show={Storage.userIsLogged() && !Storage.getCurrentUser().isBroad}
+          type='danger'
+        ></AlertMessage>
+        <div className="col-md-10">
+          <h3 style={{ fontSize: styles.titleSize, color: '#ED1D24', fontWeight: 'bold' }}>{this.state.heading}</h3>
+          <p style={{ fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}>
+            <a
+              isRendered={component.isBroad && showAccessDetails}
+              href="https://iwww.broadinstitute.org/sponsored-research/research-subject-protection/office-research-subject-protection"
+              target="_blank"
+            >ORSP on the Broad Intranet</a>
+          </p>
+          <p 
+            style={{ fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}
+            dangerouslySetInnerHTML={{ __html: this.state.body }}
+          ></p>
+          <p
+            isRendered={this.props.showWarning}
+            style={{ fontFamily : styles.fontFamily, fontSize: styles.textFontSize, padding:"15px", border:"1px solid #CCCCCC", borderRadius:"6px", margin:"20px 0 30px 0" }}
+          >Please note that Microsoft Edge and Internet Explorer are not supported browsers for the ORSP Portal. Please use Google Chrome or Firefox instead.</p>
+          <div
+            isRendered={component.isBroad && showAccessDetails}
+          >
+            <h3
+              style={{ fontSize: styles.titleSize }}
+            >User Guide</h3>
+            <p style={{ fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}>To access detailed instructions about how to use the ORSP portal, please visit: 
+              <br/>
+              <a
+                href="https://intranet.broadinstitute.org/research-subject-protection/orsp-online-portal-submission-system"
+                target="_blank"
+              >https://intranet.broadinstitute.org/research-subject-protection/orsp-online-portal-submission-system</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      // div({className: "row"}, [
+      //   AlertMessage({
+      //     msg: 'You must be a Broad Institute User for further access. Please sign out and log in with a "broadinstitute.org" email account.',
+      //     show: Storage.userIsLogged() && !Storage.getCurrentUser().isBroad,
+      //     type: 'danger'
+      //   }),
   
-        div({ className: "col-md-10" }, [
-          h3({ style: { fontSize: styles.titleSize, color: '#ED1D24', fontWeight: 'bold' }
-          },[this.state.heading]),
-          p({ style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}, [
-            a({
-              isRendered: component.isBroad && showAccessDetails,
-              href:"https://iwww.broadinstitute.org/sponsored-research/research-subject-protection/office-research-subject-protection", target: "_blank"}, [
-                "ORSP on the Broad Intranet"
-            ]),
-          ]),
-          p({style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}, [this.state.body]),
-          // p({ style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}, [
-          //   "The Office of Research Subject Protection (ORSP) at the Broad Institute is committed to helping " +
-          //   "our community members adhere to federal regulations and institutional policies governing the " +
-          //   "protection of human subjects who make our research possible. To fulfill that mission, the Broad " +
-          //   "must ensure appropriate regulatory oversight and reliable documentation storage. The ORSP " +
-          //   "Portal (accessible at ",  
-          //   a({href:"https://orsp.broadinstitute.org/"}, ["https://orsp.broadinstitute.org/"]),
-          //   "​) is an online platform where Broad staff " +
-          //   "members can upload documents for ORSP review (either as stand-alone submissions, or in " +
-          //   "preparation for an IRB protocol application), store consent forms and regulatory approvals, and " +
-          //   "search for data use restrictions. Please contact ",
-          //   a({href:"mailto:orsp-portal@broadinstitute.org"}, ["orsp-portal@broadinstitute.org"]),
-          //   " with any questions or requests for assistance.",
-          // ]),
-          p({ isRendered: this.props.showWarning, style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize, padding:"15px", border:"1px solid #CCCCCC", borderRadius:"6px", margin:"20px 0 30px 0"}},[
-            "Please note that Microsoft Edge and Internet Explorer are not supported browsers for the ORSP Portal. Please use Google Chrome or Firefox instead."
-          ]),
-          div({ isRendered:  component.isBroad && showAccessDetails}, [
-              h3({ style: { fontSize: styles.titleSize }}, ["User Guide"]),
-              p({ style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }},[
-                "To access detailed instructions about how to use the ORSP portal, please visit: ",
-                br({}),
-                a({
-                  href:"https://intranet.broadinstitute.org/research-subject-protection/orsp-online-portal-submission-system", target: "_blank"},
-                  ["https://intranet.broadinstitute.org/research-subject-protection/orsp-online-portal-submission-system"]
-                )
-              ])
-          ])
-        ])  
-      ])
+      //   div({ className: "col-md-10" }, [
+      //     h3({ style: { fontSize: styles.titleSize, color: '#ED1D24', fontWeight: 'bold' }
+      //     },[this.state.heading]),
+      //     p({ style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}, [
+      //       a({
+      //         isRendered: component.isBroad && showAccessDetails,
+      //         href:"https://iwww.broadinstitute.org/sponsored-research/research-subject-protection/office-research-subject-protection", target: "_blank"}, [
+      //           "ORSP on the Broad Intranet"
+      //       ]),
+      //     ]),
+      //     p({style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}, [this.state.body]),
+      //     // p({ style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }}, [
+      //     //   "The Office of Research Subject Protection (ORSP) at the Broad Institute is committed to helping " +
+      //     //   "our community members adhere to federal regulations and institutional policies governing the " +
+      //     //   "protection of human subjects who make our research possible. To fulfill that mission, the Broad " +
+      //     //   "must ensure appropriate regulatory oversight and reliable documentation storage. The ORSP " +
+      //     //   "Portal (accessible at ",  
+      //     //   a({href:"https://orsp.broadinstitute.org/"}, ["https://orsp.broadinstitute.org/"]),
+      //     //   "​) is an online platform where Broad staff " +
+      //     //   "members can upload documents for ORSP review (either as stand-alone submissions, or in " +
+      //     //   "preparation for an IRB protocol application), store consent forms and regulatory approvals, and " +
+      //     //   "search for data use restrictions. Please contact ",
+      //     //   a({href:"mailto:orsp-portal@broadinstitute.org"}, ["orsp-portal@broadinstitute.org"]),
+      //     //   " with any questions or requests for assistance.",
+      //     // ]),
+      //     p({ isRendered: this.props.showWarning, style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize, padding:"15px", border:"1px solid #CCCCCC", borderRadius:"6px", margin:"20px 0 30px 0"}},[
+      //       "Please note that Microsoft Edge and Internet Explorer are not supported browsers for the ORSP Portal. Please use Google Chrome or Firefox instead."
+      //     ]),
+      //     div({ isRendered:  component.isBroad && showAccessDetails}, [
+      //         h3({ style: { fontSize: styles.titleSize }}, ["User Guide"]),
+      //         p({ style: { fontFamily : styles.fontFamily, fontSize: styles.textFontSize }},[
+      //           "To access detailed instructions about how to use the ORSP portal, please visit: ",
+      //           br({}),
+      //           a({
+      //             href:"https://intranet.broadinstitute.org/research-subject-protection/orsp-online-portal-submission-system", target: "_blank"},
+      //             ["https://intranet.broadinstitute.org/research-subject-protection/orsp-online-portal-submission-system"]
+      //           )
+      //         ])
+      //     ])
+      //   ])  
+      // ])
     );
   }
 });
