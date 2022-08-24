e401e237493f09b0a4980f1a7ceec3e8da37cefd
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-08-24 15:40:02 +0530

    Update PortalMessage.js

--- src/main/webapp/components/PortalMessage.js
+++ src/main/webapp/components/PortalMessage.js
@@ -28,14 +28,14 @@ export const PortalMessage = hh(class PortalMessage extends Component {
     render() {
         return(
             <div>
-                { this.state.heading !== '' && this.state.body !== '' ?
+                { this.state.heading == '' && this.state.body == '' ? undefined :
                     <div style={{ padding:"15px", border:"1px solid #CCCCCC", borderRadius:"6px", margin:"20px 0 30px 0" }}>
                         <h3 style={{ fontSize: '24px', color: '#ED1D24', fontWeight: 'bold' }}>{this.state.heading}</h3>
                         <p 
                             style={{ fontSize: '14px' }}
                             dangerouslySetInnerHTML={{ __html: this.state.body }}
                         ></p>
-                    </div> : undefined
+                    </div>
                 }
             </div>
         )
