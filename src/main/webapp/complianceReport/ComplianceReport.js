4d7cd2d63883a0e6a9e969eec8bf20bd67c73248
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-10-03 20:50:41 +0530

    Cric 1263 compliance report (#1072)
    * Compliance report data api
    
    * Compliance report data api
    
    * Compliance report
    
    * Update ComplianceReport.js
    
    * Update Routes.js
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Update ComplianceReport.js
    
    * Compliance report data api
    
    * Update ajax.js
    
    * Compliance report data api
    
    * Update ajax.js
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Update ajax.js
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Update ComplianceReport.js
    
    * Compliance report data api
    
    * Compliance report data api
    
    * Update QaReport.js
    
    * Compliance report data api
    
    * Update ComplianceReport.js

--- src/main/webapp/complianceReport/ComplianceReport.js
+++ src/main/webapp/complianceReport/ComplianceReport.js
@@ -5,28 +5,83 @@ class ComplianceReport extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            data: []
+            data: [],
+            afterDate: '',
+            beforeDate: '',
+
         }
     }
 
-    componentDidMount() {
-        this.init();
+    applyFilterPanel = () => {
+        Reports.getComplianceReportData(this.state.afterDate, this.state.beforeDate).then(data => {
+            console.log(data);
+        })
     }
 
-    async init() {
-        await Reports.getComplianceReportData('2021-01-01', '2022-09-28').then(data => {
-            console.log(data)
-            this.setState({
-                data: data
-            })
+    handleAfterDatePicker = (e) => {
+        console.log('afterDate: ', e)
+        this.setState({
+            afterDate: (e.target.value).toString()
+        })
+    }
+
+    handleBeforeDatePicker = (e) => {
+        console.log('afterDate: ', e)
+        this.setState({
+            afterDate: (e.target.value).toString()
+        })
+    }
+
+    clearFilterPanel = () => {
+        this.setState({
+            afterDate: '',
+            beforeDate: ''
         })
     }
 
     render() {
         return(
-            <div>
-                <h1>Compliance Report</h1>
-            </div>
+            div({
+            },[
+                div({}, [
+                    <h1>Compliance Report</h1>
+                ]),
+                Panel({ title: "Filter Compliance report" }, [
+                    div({className: "row"}, [
+                    div({className: "col-xs-12 col-sm-6"}, [
+                        InputFieldDatePicker({
+                        selected: this.state.afterDate,
+                        name: "afterDate",
+                        label: "Created After",
+                        onChange:  (e) => this.handleAfterDatePicker(e),
+                        disabled: false
+                        })
+                    ]),
+                    div({className: "col-xs-12 col-sm-6"}, [
+                        InputFieldDatePicker({
+                        selected: this.state.beforeDate,
+                        name: "beforeDate",
+                        label: "Created Before",
+                        onChange: (e) => this.handleBeforeDatePicker(e),
+                        disabled: false
+                        })
+                    ])
+                    ]),
+                    button({
+                    className: "btn buttonPrimary",
+                    style: { marginTop: '20px', marginRight: '10px' },
+                    onClick: this.applyFilterPanel
+                    }, ['Filter']),
+                    button({
+                    className: "btn buttonSecondary",
+                    style: { marginTop: '20px' },
+                    onClick: this.clearFilterPanel
+                    }, ['Clear'])
+                ]),
+                <div>
+
+                </div>
+            ])
         )
     }
 }
