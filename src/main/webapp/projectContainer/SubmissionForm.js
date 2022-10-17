69e3188bc9b21d0dddd0c7c6ffae4fa0be2f0fe3
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-10-17 16:16:44 +0530

    Cric 1431 drag and drop (#1130)
    * Drag and drop
    
    * Update package.json
    
    * adding react-drag-drop-files
    
    * adding react-drag-drop-files
    
    * Update package.json
    
    * Drag and drop
    
    * Update package-lock.json
    
    * Update SubmissionForm.js
    
    * Drag and drop

--- src/main/webapp/projectContainer/SubmissionForm.js
+++ src/main/webapp/projectContainer/SubmissionForm.js
@@ -340,6 +340,7 @@ const SubmissionForm = hh(class SubmissionForm extends Component {
         })
     }
     this.setState(prev => {
+      console.log('submission file: ', file)
       prev.dropEvent = file
     }, () => {
       this.addDocuments();
@@ -453,7 +454,7 @@ const SubmissionForm = hh(class SubmissionForm extends Component {
               onDragOver: this.dragOverHandler,
               style: {padding: '10px 0 10px 0', textAlign: 'center', border: '1px solid #ddd', width: '100%'}
             }, [
-              p(['Drag and drop your documents here or', a(['click here to add documents'])])
+              p(['Drag and drop your documents here or ', a({onClick:() => {this.addDocuments()}}, ['click here to add documents'])])
             ])
           //   button({
           //     isRendered: !component.isViewer,
