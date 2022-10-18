e1444b6ec04c9ff88b99cfc8e3367fa40c98a19e
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-10-18 19:01:46 +0530

    CRIC-1440 - FindUsers parameter type error fix (#1136)

--- grails-app/services/org/broadinstitute/orsp/NotifyService.groovy
+++ grails-app/services/org/broadinstitute/orsp/NotifyService.groovy
@@ -659,7 +659,9 @@ class NotifyService implements SendgridSupport, Status {
 
     Map<Boolean, String> sendApprovedNotification(Issue issue, String sessionUsername) {
         Collection<User> usersToNotify = userService.findUsers(issue.getPMs())
-        Collection<User> reportersToNotify = userService.findUsers(issue.getReporter())
+        Collection<String> reporter
+        reporter.addAll(issue.getReporter())
+        Collection<User> reportersToNotify = userService.findUsers(reporter)
         Collection<String> pmEmails = usersToNotify.emailAddress
         Collection<String> reporterEmails = reportersToNotify.emailAddress
         Collection<String> emails
