4a9d96b0b663a1966fdefc37695d7a86de36b087
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-10-21 17:17:04 +0530

    Cric 1431 drag drop (#1153)
    * Drag and drop for attachments
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop

--- src/main/webapp/components/AddDocumentDialog.js
+++ src/main/webapp/components/AddDocumentDialog.js
@@ -47,6 +47,7 @@ const AddDocumentDialog = hh(class AddDocumentDialog extends Component{
     this.setState({
       dropEvent: this.props.dropEvent
     })
+    this.setDroppedFilesToUpload();
   }
 
   handleClose = () => {
@@ -84,6 +85,7 @@ const AddDocumentDialog = hh(class AddDocumentDialog extends Component{
           this.setDroppedFilesToUpload();
           file = { file: this.state.dropEvent, fileKey: this.state.type.label }
         } else {
+          console.log('upload else worked')
           file = { file: this.state.file, fileKey: this.state.type.label };
         }
         let files = [file];
