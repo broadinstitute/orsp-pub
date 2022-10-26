5b3e8fa438d5b729ec3d65fd878c7225a684471b
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-10-26 17:52:58 +0530

    Cric 1431 drag drop (#1175)
    * Drag and drop for attachments
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Update AddDocumentDialog.js
    
    * Update AddDocumentDialog.js
    
    * Update SubmissionForm.js
    
    * Update SubmissionForm.js
    
    * Update AddDocumentDialog.js
    
    * Update AddDocumentDialog.js
    
    * Update SubmissionForm.js
    
    * Update SubmissionForm.js
    
    * Drag and drop for attachments
    
    * Drag and drop for attachments
    
    * Drag and drop for attachments
    
    * Drag and drop
    
    * Update AddDocumentDialog.js
    
    * Drag and drop for attachments
    
    * Drag and drop
    
    * Drag and drop for attachments
    
    * Drag and drop for attachments
    
    * Update SubmissionForm.js
    
    * Modal sstyle change
    
    * drag and drop for attachments
    
    * Modal design change
    
    * Drag and drop

--- src/main/webapp/project/NewProjectDocuments.js
+++ src/main/webapp/project/NewProjectDocuments.js
@@ -151,7 +151,6 @@ export const NewProjectDocuments = hh(class NewProjectDocuments extends Componen
               documentHandler: this.setFilesToUpload,
               dropEvent: this.state.dropEvent
             }),
-            br(),br(),
             div({ style: styles.addDocumentContainer }, [
               div({
                 isRendered: !component.isViewer,
