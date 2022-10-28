8fc02021032768d662ef3f710e0d560bef46c3fa
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-10-28 16:43:32 +0530

    Cric 1431 drag drop (#1195)
    * Drag and drop for attachments
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Drag and drop
    
    * Update AddDocumentDialog.js
    
    * Update AddDocumentDialog.js
    
    * Update SubmissionForm.js
    
    * Update SubmissionForm.js
    
    * Update AddDocumentDialog.js
    
    * Update AddDocumentDialog.js
    
    * Update SubmissionForm.js
    
    * Update SubmissionForm.js
    
    * Drag and drop for attachments
    
    * Drag and drop for attachments
    
    * Drag and drop for attachments
    
    * Drag and drop
    
    * Update AddDocumentDialog.js
    
    * Drag and drop for attachments
    
    * Drag and drop
    
    * Drag and drop for attachments
    
    * Drag and drop for attachments
    
    * Update SubmissionForm.js
    
    * Modal sstyle change
    
    * drag and drop for attachments
    
    * Modal design change
    
    * Drag and drop
    
    * Adding description for attachments
    
    * Adding description to attachments
    
    * Adding description for attachments in SubmissionController
    
    * Adding description to submission attachment
    
    * Adding file description
    
    * Addition columns for document details table
    
    * Document table column change
    
    * Update SubmissionForm.js
    
    * Update AddDocumentDialog.js
    
    * Document table column change
    
    * Update AddDocumentDialog.js
    
    * Adding new columns to document table view
    
    * Adding new columns to document table view
    
    * Update SubmissionForm.js
    
    * Adding new columns to document view table
    
    * Adding new columns to document table
    
    * Adding new columns to document table
    
    * Adding description column to document table
    
    * Update Documents.js
    
    * Update Documents.js
    
    * Update Documents.js
    
    * Adding description for attachments in FileHelperController
    
    * Adding description to document data

--- grails-app/controllers/org/broadinstitute/orsp/api/FileHelperController.groovy
+++ grails-app/controllers/org/broadinstitute/orsp/api/FileHelperController.groovy
@@ -1,9 +1,13 @@
 package org.broadinstitute.orsp.api
 
 import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonElement
+import com.google.gson.JsonParser
 import grails.converters.JSON
 import grails.rest.Resource
 import org.apache.commons.collections.CollectionUtils
+import org.apache.commons.fileupload.disk.DiskFileItem
 import org.broadinstitute.orsp.AuthenticatedController
 import org.broadinstitute.orsp.ConsentCollectionLink
 import org.broadinstitute.orsp.ConsentService
@@ -20,6 +24,14 @@ class FileHelperController extends AuthenticatedController{
     ConsentService consentService
 
     def attachDocument() {
+        JsonParser parser = new JsonParser()
+        JsonElement jsonFileDescription = parser.parse(request?.parameterMap["fileDescription"].toString())
+        JsonArray fileData
+
+        if (jsonFileDescription.jsonArray) {
+            fileData = jsonFileDescription.asJsonArray
+        }
+
         List<MultipartFile> files = request.multiFileMap.collect { it.value }.flatten()
 
         def names = []
@@ -38,7 +50,8 @@ class FileHelperController extends AuthenticatedController{
                             creator: params.displayName,
                             username: params.userName,
                             creationDate: new Date(),
-                            status: DocumentStatus.PENDING.status
+                            status: DocumentStatus.PENDING.status,
+                            description: fileData.find {data -> data.fileName.value == it.originalFilename }.fileDescription.value
                     )
                     storageProviderService.saveStorageDocument(document, it.getInputStream())
                     persistenceService.saveEvent(issue.projectKey, getUser()?.displayName, "Document Added", EventType.UPLOAD_DOCUMENT)
