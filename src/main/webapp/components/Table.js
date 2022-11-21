1349454cd0646919352bc7c6a43e999a825c39e9
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-11-21 15:19:49 +0530

    Cric 1452 document description edit (#1282)
    * Adding description to submission table

--- src/main/webapp/components/Table.js
+++ src/main/webapp/components/Table.js
@@ -36,12 +36,6 @@ const styles = {
   }
 };
 
-const cellEditProp = {
-  mode: 'dbclick',
-  blurToSave: true,
-  afterSaveCell: this.props.onAfterSaveCell  // a hook for after saving cell
-};
-
 export const Table = hh(class Table extends Component {
 
   constructor(props) {
@@ -50,7 +44,7 @@ export const Table = hh(class Table extends Component {
       cellEditProp: {
         mode: 'dbclick',
         blurToSave: true,
-        afterSaveCell: onAfterSaveCell  // a hook for after saving cell
+        afterSaveCell: this.props.onAfterSaveCell  // a hook for after saving cell
       }
     }
     this.formatUrlDocument = this.formatUrlDocument.bind(this);
