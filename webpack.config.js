038792130af35bc7258f0472c6649ba7927ca6e1
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-11-22 14:08:18 +0530

    Cric 1452 document description edit (#1305)
    * xlsx cptable not defined error fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  
   node: {
     global: true,
     __filename: true,
@@ -11,6 +12,7 @@ module.exports = {
       fs: false
     }
   },
+
   entry: {
     linkWizard: './src/main/webapp/linkWizard/index.js',
     mainIndex: './src/main/webapp/main/main_index.js'
