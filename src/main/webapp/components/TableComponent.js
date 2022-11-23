9fbb8f7f30cc7d30b7472a2d0519d8c6f6883ee6
Akhil S A <109232859+saakhil@users.noreply.github.com>
2022-11-23 15:46:05 +0530

    Adding new excel export library
    - sheetjs-style library for exporting excel
    - Export code change

--- src/main/webapp/components/TableComponent.js
+++ src/main/webapp/components/TableComponent.js
@@ -9,7 +9,7 @@ import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import './Btn.css';
 import './TableComponent.css'
-import { Export } from "./Export";
+import Export from "./Export";
 import { EXPORT_FILE } from "../util/TableUtil";
 import { TableHeaderColumn } from 'react-bootstrap-table';
 
@@ -37,14 +37,11 @@ export const TableComponent = hh(class TableComponent extends Component {
               {this.props.showSearchBar ? <SearchBar { ...props.searchProps } /> : ''}
               {this.props.showExportButtons ?
                 <span name={'exportButtons'}>
-                  {/* <Export
-                    csvData={this.props.data}
-                    columns={this.props.columns}
+                  <Export
+                    excelData={this.props.data}
                     fileName={this.props.fileName}
-                    fileType={EXPORT_FILE.XLSX.mimeType}
-                    fileExtension={EXPORT_FILE.XLSX.extension}
-                    hide={this.props.hideXlsxColumns}
-                  /> */}
+                    btnClassName={ "btn buttonSecondary pull-right" }
+                  />
                   <ExportCSVButton className={"pull-right"} { ...props.csvProps }>
                     <span>
                       <i style={{ marginRight:'5px' }} className= { "fa fa-download" }></i> Download CSV
