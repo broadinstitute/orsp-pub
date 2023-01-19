87023127d2c4d7c26dd2423a3fceec61dcd118e9
Akhil S A <109232859+saakhil@users.noreply.github.com>
2023-01-19 16:14:41 +0530

    Cric 1478 google auth upgrade (#1359)
    * Google authentication upgrade

--- src/main/webapp/components/GoogleLoginAuth.js
+++ src/main/webapp/components/GoogleLoginAuth.js
@@ -1,31 +1,21 @@
-import React, { Component } from "react";
-import { hh } from 'react-hyperscript-helpers'
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
 
-const GoogleLoginAuth = hh( class GoogleLoginAuth extends Component {
+function GoogleAuth() {
+    return (
+        <div>
+            <GoogleOAuthProvider clientId={component.clientId} >
+            <GoogleLogin 
+                onSuccess={credentialResponse => {
+                    this.props.onSuccess(credentialResponse)
+                  }}
+                  onError={(err) => {
+                    console.log(err, 'Login Failed');
+                  }}
+            />
+            </GoogleOAuthProvider>
+        </div>
+    )
+}
 
-    onSuccess = (response) => {
-        this.props.onSuccess(response.credential);
-    }
-
-    componentDidMount = () => {
-        /* global google */
-        google.accounts.id.initialize({
-          client_id: component.client_id,
-          callback: onSuccess
-        });
-    
-        google.accounts.id.renderButton(
-          document.getElementById("signInDiv"),
-          { theme: "outline", size: "large" }
-        )
-    }
-
-    render() {
-        return (
-            <div id="signInDiv"></div>
-        )
-    }
-
-})
-
-export default GoogleLoginAuth
+export default GoogleAuth
