a04c0065e1a37641e0442c5fe23ae10883e3d3ba
Akhil S A <109232859+saakhil@users.noreply.github.com>
2023-01-19 15:51:09 +0530

    Update GoogleLoginAuth.js

--- src/main/webapp/components/GoogleLoginAuth.js
+++ src/main/webapp/components/GoogleLoginAuth.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { hh } from 'react-hyperscript-helpers'
 
 const GoogleLoginAuth = hh( class GoogleLoginAuth extends Component {
