498899807d207001620aa0f35d5675a3e14cdb6b
John Winsley <109232859+saakhil@users.noreply.github.com>
2023-06-13 12:43:12 +0530

    CRIC-1632
    Make comment editable and delete-able

--- src/main/webapp/components/Comments.js
+++ src/main/webapp/components/Comments.js
@@ -160,9 +160,7 @@ const Comments = hh(class Comments extends Component {
 
   render() {
     return (
-      h(Fragment, {
-        id: 'comment',
-      }, [
+      h(Fragment, [
         h(TextEditor, {
           isRendered: !this.state.editMode,
           id: this.props.id,
@@ -170,7 +168,8 @@ const Comments = hh(class Comments extends Component {
         }),
         div({
           isRendered: this.state.editMode,
-          className: "well"
+          className: "well",
+          id: 'comment',
         },[
           label({},["Edit comment"]),
           h(Editor, {
