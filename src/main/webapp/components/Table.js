34999d3d2f68c404b69765e04a2baa50dcdb342c
amaljithbroad <amaljith@broadinstitute.org>
2023-06-20 10:17:34 +0530

    Cric 1636 redesign submission tab text layout (#1518)
    * Added actions column and moved Edit button
    
    Added actions column and moved Edit button to it
    
    * Set width of each columns

--- src/main/webapp/components/Table.js
+++ src/main/webapp/components/Table.js
@@ -14,6 +14,8 @@ import { Link } from 'react-router-dom';
 const styles = { 
   statusWidth: '140',
   fileTypeWidth: '170',
+  fileDescription: '7%',
+  fileName: '5%',
   userNameWidth: '180',
   docVersionWidth: '90',
   creatorWidth: '130',
@@ -23,11 +25,12 @@ const styles = {
   removeWidthFile: '80',
   unlinkSampleCollectionWidth: '80',
   collectionNameWidth: '270',
-  numberWidth: '85',
+  numberWidth: '2%',
   createDateWidth: '15',
   submissionDocumentsWidth: '200',
-  submissionComments: '200',
-  createdWidth: '120',
+  submissionComments: '20%',
+  submissionActions: '3%',
+  createdWidth: '4%',
   linkOverflowEllipsis: {
     whiteSpace: 'nowrap',
     overflow: 'hidden',
@@ -272,7 +275,7 @@ export const Table = hh(class Table extends Component {
                 key={header.name}
                 dataField={header.value}
                 dataSort={true}
-              >{header.name}</TableHeaderColumn>
+                width={styles.fileDescription}>{header.name}</TableHeaderColumn>
             }
             if (header.value === 'status') {
               return <TableHeaderColumn key={header.name}
@@ -318,7 +321,8 @@ export const Table = hh(class Table extends Component {
                 dataField={header.value}
                 dataFormat={this.formatUrlDocument}
                 editable={ false }
-                dataSort={true}>{header.name}</TableHeaderColumn>
+                dataSort={true}
+                width={styles.fileName}>{header.name}</TableHeaderColumn>
             } else if (header.value === 'projectKey') {
               return <TableHeaderColumn isKey={isKey}
                 key={header.name}
@@ -436,7 +440,7 @@ export const Table = hh(class Table extends Component {
                 editable={ false }
                 dataFormat={this.submissionEditActions}
                 dataSort={ false }
-                width={styles.createdWidth}>{header.name}</TableHeaderColumn>
+                width={styles.submissionActions}>{header.name}</TableHeaderColumn>
             } else {
               return <TableHeaderColumn isKey={isKey}
                 key={header.name}
