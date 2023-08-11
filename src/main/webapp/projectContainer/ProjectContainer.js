a31d851b1bab60162d44d79735fa12497490e773
Akhil S A <109232859+saakhil@users.noreply.github.com>
2023-08-11 15:26:00 +0530

    Cric 1672 irb of record field clearable (#1666)
    * CRIC-1672
    
    Clear IRB-of-Record field

--- src/main/webapp/projectContainer/ProjectContainer.js
+++ src/main/webapp/projectContainer/ProjectContainer.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, createRef } from 'react';
 import { div, h, hh } from 'react-hyperscript-helpers';
 import ProjectReview from '../projectReview/ProjectReview';
 import { History } from '../components/History';
@@ -26,6 +26,7 @@ export const ProjectContainer = hh(class ProjectContainer extends Component {
       dialogContent: '',
       activeTab: 'review'
     };
+    this.child = createRef();
   }
 
   componentDidMount= async () => {
@@ -184,6 +185,7 @@ export const ProjectContainer = hh(class ProjectContainer extends Component {
                     updateAdminOnlyStatus: this.updateAdminOnlyStatus,
                     initStatusBoxInfo: this.props.initStatusBoxInfo,
                     projectKey: this.props.projectKey,
+                    onClick: () => { this.child.current.init() }
                   })
                 ])
             ])
