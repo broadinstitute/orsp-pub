152b7e2a02b8ad3e9929aee744e3aab8ef38e359
Akhil S A <109232859+saakhil@users.noreply.github.com>
2023-08-14 12:02:04 +0530

    Saving history based on previous status

--- grails-app/services/org/broadinstitute/orsp/IssueService.groovy
+++ grails-app/services/org/broadinstitute/orsp/IssueService.groovy
@@ -461,7 +461,7 @@ class IssueService implements UserInfo {
         }
         if (shouldUpdateStatus(input.get(IssueExtraProperty.PROJECT_STATUS), previousStatus)) {
             persistenceService.saveEvent(issue.projectKey, getUser()?.displayName, "Project " + input.get(IssueExtraProperty.PROJECT_STATUS), eventTypeMatcher(input.get(IssueExtraProperty.PROJECT_STATUS)))
-        } else if (input.get(IssueExtraProperty.PROJECT_STATUS) == "On Hold") {
+        } else if (input.get(IssueExtraProperty.PROJECT_STATUS) == "On Hold" && previousStatus != "On Hold") {
             persistenceService.saveEvent(issue.projectKey, getUser()?.displayName, "Project On Hold", EventType.ONHOLD_PROJECT)
         }
         String newStatus = Optional.ofNullable(input.get(IssueExtraProperty.PROJECT_STATUS)).orElse("")
