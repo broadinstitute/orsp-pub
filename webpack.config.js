a9ff396530e6236ac2f4f8b415ccb843990d7cb9
amaljithbroad <amaljith@broadinstitute.org>
2023-09-20 05:55:40 -0700

    CRIC-1262 Technical upgrade (#1706)
    Added webpack-cli, webpack config edited

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,45 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-
   node: {
     global: true,
     __filename: true,
     __dirname: true,
   },
-  resolve: {
-    fallback: {
-      fs: false
-    }
-  },
-  
+  resolve: {},
+
   entry: {
-    linkWizard: './src/main/webapp/linkWizard/index.js',
-    mainIndex: './src/main/webapp/main/main_index.js'
+    linkWizard: "./src/main/webapp/linkWizard/index.js",
+    mainIndex: "./src/main/webapp/main/main_index.js",
   },
   output: {
-    path: path.join(__dirname, 'grails-app/assets/javascripts/build'),
-    publicPath: '/assets/',
-    filename: '[name].js'
+    path: path.join(__dirname, "grails-app/assets/javascripts/build"),
+    publicPath: "/assets/",
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.join(__dirname, 'src/main/webapp'),
+        include: path.join(__dirname, "src/main/webapp"),
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ["react", ["es2015", {"modules": false}]],
-            plugins: ["transform-class-properties"]
-          }
-        }
+            presets: ["react", ["es2015", { modules: false }]],
+            plugins: ["transform-class-properties"],
+          },
+        },
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: {
-          loader: 'url-loader?limit=10000&prefix=assets/!img'
-        }
-      }
-    ]
-  }
+          loader: "url-loader?limit=10000&prefix=assets/!img",
+        },
+      },
+    ],
+  },
 };
-
