e29cd4835f025c59e5d785c93aa50089f0e2f290
Akhil S A <109232859+saakhil@users.noreply.github.com>
2024-04-18 18:53:52 +0530

    Cric 1880 new irb report request (#1824)
    * CRIC-1880 New IRB Report
    
    - IRB Report
    - Added unique id for keyfield
    - Changed query condition from approval status to status
    - Data inconsistency issue fix
    - Change in funding data display form
    - Sort data based on requested date

--- grails-app/services/org/broadinstitute/orsp/QueryService.groovy
+++ grails-app/services/org/broadinstitute/orsp/QueryService.groovy
@@ -1996,7 +1996,7 @@ class QueryService implements Status {
                                     .append("SELECT project_key, CONCAT(GROUP_CONCAT(CONCAT('\"', source, '\":\"', name, '\"'))) as funding_source ")
                                     .append("FROM funding GROUP BY project_key) t3 ON t1.project_key = t3.project_key ")
                                     .append("WHERE t1.type = 'IRB Project' AND (t1.status = 'Approved' OR t1.approval_status = 'Approved') ")
-                                    .append("GROUP BY t1.project_key;")
+                                    .append("GROUP BY t1.project_key, t1.request_date ORDER BY t1.request_date;")
         final SQLQuery sqlQuery = session.createSQLQuery(query)
         final result = sqlQuery.with {
             list()
