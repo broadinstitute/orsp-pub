c3b906ce3c21c35132304f4b54a1a88ac9009abd
Akhil S A <109232859+saakhil@users.noreply.github.com>
2025-01-23 15:36:25 +0530

    Cric 2026 retain edited project data (#1898)
    * CRIC-2026: Retain edited project data
    
    * Revert Project Versioning

--- sql-scripts/cric-2042/consent-collection-link.sql
+++ sql-scripts/cric-2042/consent-collection-link.sql
@@ -1,6 +1,6 @@
 alter table consent_collection_link 
-add data_secondary_use varchar(255) default null;
-add collaborator_approval varchar(255) default null;
+add data_secondary_use varchar(255) default null,
+add collaborator_approval varchar(255) default null,
 add mta_or_dta varchar(255) DEFAULT NULL after collaborator_approval,
 add delivery_date datetime(6) DEFAULT NULL after mta_or_dta,
 add release_date datetime(6) DEFAULT NULL after delivery_date,
